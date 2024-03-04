362d079900e6a9b36300859700e7f87df2e10ea0
João vitor <joaovitorcfdev@gmail.com>
2024-03-03 21:36:08 -0300

    estrutura de controle

---
+++ 11_estrutura_de_controle/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	numero := 10
+
+	if numero > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor ou igual a 15")
+	}
+
+	if outroNumero := numero; outroNumero > 0 {
+		fmt.Println("Maior que zero")
+	}
+}
